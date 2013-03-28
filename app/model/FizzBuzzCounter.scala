6ecd67618176ae74fcbe11f5d9f04c502b1815c0
locatedtaco <lkam88@gmail.com>
2013-03-27 22:52:37 -0700

    Limited counting to 9999.  Fixed the default fizzbuzz uri

--- app/model/FizzBuzzCounter.scala
+++ app/model/FizzBuzzCounter.scala
@@ -4,18 +4,23 @@ object FizzBuzzCounter {
 
   def fizzBuzz(number: Int): List[String] = {
     val buffer = new scala.collection.mutable.ListBuffer[String]
-    for (x <- 1 to number) {
-      var result = ""
-      if (x % 3 == 0) {
-        result += "Fizz"
+    if (number > 10000) {
+      buffer += "Number too large!"
+    }
+    else {
+      for (x <- 1 to number) {
+        var result = ""
+        if (x % 3 == 0) {
+          result += "Fizz"
+        }
+        if (x % 5 == 0) {
+          result += "Buzz"
+        }
+        if (result == "") {
+          result += x
+        }
+        buffer += result;
       }
-      if (x % 5 == 0) {
-        result += "Buzz"
-      }
-      if (result == "") {
-        result += x
-      }
-      buffer += result;
     }
     buffer.toList
   }
